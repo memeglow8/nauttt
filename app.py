0adf205531fba49b4918bdfbe58808dc33592dc1
memeglow8 <memeglow8@gmail.com>
2024-10-29 22:35:02 -0800

    Update app.py new

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
+import os
 from flask import Flask
 from routes import app as routes_blueprint
 from database import init_db, restore_from_backup
 from helpers import send_startup_message
 from config import Config
-import os
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = Config.SECRET_KEY  # Use SECRET_KEY from Config for consistency
-    
-    # Register the Blueprint for routes
-    app.register_blueprint(routes_blueprint, url_prefix="/")
-    
-    # Initialize the database and perform any required startup actions
-    init_db()  # Initialize the database tables if not present
-    restore_from_backup()  # Check and notify total tokens in the database
-    send_startup_message()  # Send startup message via Telegram to notify app is running
-    
+    app.secret_key = Config.SECRET_KEY
+    app.register_blueprint(routes_blueprint)  # Register routes blueprint
+    init_db()  # Initialize database
+    restore_from_backup()  # Restore tokens from backup if needed
+    send_startup_message()  # Send startup message via Telegram
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    # Define the host and port for running the application
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
